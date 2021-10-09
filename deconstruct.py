b205603f6868dd957343ed96e2834ee37213bf66
Perice Pope <perice09@gmail.com>
2021-10-09 00:21:28 +0000

    notes for practice

--- deconstruct.py
+++ deconstruct.py
@@ -1,6 +1,16 @@
 # pull apart here... cheers
-def insertionSort(nums): 
-  print(nums)
 
-nums = [1,3,2,4,6,2,3,89]
-insertionSort()
+def insertionSort(nums): 
+  # print(nums)
+  for i in range(1,len(nums)):
+    key = nums[i] # each element 
+    j = i-1 # the item to the left of i 
+    print(nums[j+1])
+
+nums = [1,25,0]
+insertionSort(nums)
+
+		# Move elements of arr[0..i-1], that are
+		# greater than key, to one position ahead
+		# of their current position
+    # move things forward and backwards for fun! :D We movin baby! 
